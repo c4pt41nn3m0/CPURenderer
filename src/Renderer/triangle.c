60e32a946481ffe0afd4209b1b3b47aaffec66ff
Karanveer Thakur <thakur.singh.karanveer@gmail.com>
2024-06-11 15:58:35 +0530

    - Fixed bug with division by zero if in draw filled triangle function either y1 is equal to y2 or y0 is equal to y1

--- src/Renderer/triangle.c
+++ src/Renderer/triangle.c
@@ -45,16 +45,25 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         int_swap(&x0, &x1);
     }
 
-    // Calculate vertex (Mx, My) using triangle similarity
-    int My = y1;
-    int Mx = x0 + ((float)(x2 - x0) * (y1 - y0) / (float)(y2 - y0));
+    // Only draw flat bottom triangle if y1 == y2
+    if (y1 == y2) {
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    }
+    //Only draw flat top triangle 
+    else if (y0 == y1) {
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    }
+    else {
+        // Calculate vertex (Mx, My) using triangle similarity
+        int My = y1;
+        int Mx = x0 + ((float)(x2 - x0) * (y1 - y0) / (float)(y2 - y0));
 
-    // Draw flat bottom triangle (x0, y0), (x1, y1), (Mx, My)
-    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+        // Draw flat bottom triangle (x0, y0), (x1, y1), (Mx, My)
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
 
-    // Draw flat top triangle
-    fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
-
+        // Draw flat top triangle
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
 }
 
 void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
